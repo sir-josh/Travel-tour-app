91ba2e06271a5a5e1ccf4ba4483f9532e0ac4172
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-04 15:46:47 +0100

    Init: initialize file setup and project scafolding

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+
+const App = () => {
+
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="product" element={<Product />} />
+				<Route path="pricing" element={<Pricing />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default App;
