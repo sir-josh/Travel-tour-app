ec8f761c202f1325fd7a757c314376cca74ddf2a
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-06 11:02:01 +0100

    feat: read & set query string, perform programmatic navigation with useNavigate on page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -8,6 +9,8 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -60,7 +63,8 @@ const App = () => {
 							/>
 						}
 					/>
-					<Route path="form" element={<p>Form</p>} />
+					<Route path="cities/:id" element={<City />} />
+					<Route path="form" element={<Form />} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
