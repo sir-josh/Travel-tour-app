a9cae9153891f4fa807458c272d5e55a9c1c7dd2
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-09 01:54:33 +0100

    feat: add protected routes feature

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/AuthContext";
+import ProctectedRoutes from "./pages/ProctectedRoutes";
 
 const App = () => {
 	return (
@@ -24,7 +25,13 @@ const App = () => {
 						<Route path="/product" element={<Product />} />
 						<Route path="/pricing" element={<Pricing />} />
 						<Route path="/login" element={<Login />} />
-						<Route path="/app" element={<AppLayout />}>
+						<Route
+							path="/app"
+							element={
+								<ProctectedRoutes>
+									<AppLayout />
+								</ProctectedRoutes>
+							}>
 							{/* <Route index element={<CityList />} /> */}
 							<Route
 								index
