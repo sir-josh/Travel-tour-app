cffdd9b918bddb40fdd389297eda63ad31efd758
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-13 01:12:08 +0100

    refactor: optimize city component context

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import {
+	createContext,
+	useCallback,
+	useContext,
+	useEffect,
+	useReducer,
+} from "react";
 
 const CitiesContext = createContext();
 const BASE_URL = "http://localhost:8000";
@@ -74,21 +80,24 @@ function CitiesProvider({ children }) {
 		fetchCities();
 	}, []);
 
-	async function getCity(id) {
-		if (Number(id) === currentCity.id) return;
+	const getCity = useCallback(
+		async function getCity(id) {
+			if (Number(id) === currentCity.id) return;
 
-		dispatch({ type: "loading" });
-		try {
-			const res = await fetch(`${BASE_URL}/cities/${id}`);
-			const data = await res.json();
-			dispatch({ type: "currentCity/loaded", payload: data });
-		} catch {
-			dispatch({
-				type: "rejected",
-				payload: "there was an error, refresh page and try again",
-			});
-		}
-	}
+			dispatch({ type: "loading" });
+			try {
+				const res = await fetch(`${BASE_URL}/cities/${id}`);
+				const data = await res.json();
+				dispatch({ type: "currentCity/loaded", payload: data });
+			} catch {
+				dispatch({
+					type: "rejected",
+					payload: "there was an error, refresh page and try again",
+				});
+			}
+		},
+		[currentCity.id],
+	);
 
 	async function addNewCity(newcity) {
 		dispatch({ type: "loading" });
